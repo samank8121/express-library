599062ad7f7c005f0f8933ecc891af52b5fc665a
saman <saman.k8121@gmail.com>
2025-04-03 13:19:45 +0300

    chore: add swagger merger

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
-
+swaggerDocs(app);
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found"));
 });
@@ -30,5 +30,4 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);  
-  swaggerDocs(app);
 });
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,18 +1,33 @@
-import { Express, Request, Response } from "express";
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-
-const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
+import { Express, Request, Response } from 'express';
+import yaml from 'yamljs';
+import path from 'path';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import { mergeSwaggerYaml } from './swagger-yaml-merger';
 
 function swaggerDocs(app: Express) {
-  // Swagger page
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  const outputPath = path.join(__dirname, '../swagger/swagger.yaml');
+  mergeSwaggerYaml(
+    path.join(__dirname, '../swagger/base.yaml'),
+    path.join(__dirname, '../swagger/paths'),
+    path.join(__dirname, '../swagger/schemas'),
+    outputPath
+  );
 
-  // Docs in JSON format
-  app.get("/api-docs.json", (req: Request, res: Response) => {
-    res.setHeader("Content-Type", "application/json");
-    res.json(swaggerDocument);
-  });
+  // Check if file exists before loading
+  if (fs.existsSync(outputPath)) {
+    // Swagger page
+    const swaggerDocument = yaml.load(outputPath);
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    // Docs in JSON format
+    app.get('/api-docs.json', (req: Request, res: Response) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(swaggerDocument);
+    });
+  } else {
+    console.error('Swagger documentation file not found!');
+  } 
 }
 
 export default swaggerDocs;
